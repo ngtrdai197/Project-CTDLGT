33f07980d3a198d83fdfc4b53f29da863f439575
Dai Nguyen <ngtrdai290197@gmail.com>
2020-07-07 00:38:52 +0700

    feat(Mon Hoc): add randomId | remove mon hoc by id

--- QUANLY_DIEM_SV/Source.cpp
+++ QUANLY_DIEM_SV/Source.cpp
@@ -20,16 +20,17 @@ struct mon_hoc
 };
 typedef struct mon_hoc MON_HOC;
 
-struct ds_monhoc {
-	MON_HOC* ds[MAX_MON_HOC];
+struct ds_monhoc
+{
+	MON_HOC *ds[MAX_MON_HOC];
 	int n = 0;
 };
 typedef struct ds_monhoc DS_MON_HOC;
 // ===== END MON HOC =====
 
-
 // ===== BEGIN SINH VIEN =====
-struct sinh_vien {
+struct sinh_vien
+{
 	// data
 	char MASV[12];
 	char PHAI[4];
@@ -39,19 +40,21 @@ struct sinh_vien {
 	string TEN;
 	int NAMNHAPHOC;
 	// pointer
-	sinh_vien* pNext;
+	sinh_vien *pNext;
 };
 typedef struct sinh_vien SINH_VIEN;
 
-struct ds_sinh_vien {
-	SINH_VIEN* pHead = NULL;
-	SINH_VIEN* pTail = NULL;
+struct ds_sinh_vien
+{
+	SINH_VIEN *pHead = NULL;
+	SINH_VIEN *pTail = NULL;
 };
 typedef struct ds_sinh_vien DS_SINH_VIEN;
 // ===== END SINH VIEN =====
 
 // ===== BEGIN LOP TIN CHI =====
-struct lop_tin_chi {
+struct lop_tin_chi
+{
 	// data
 	int MALOPTC;
 	char MAMH[12];
@@ -62,12 +65,13 @@ struct lop_tin_chi {
 	int sv_min;
 
 	// pointer
-	lop_tin_chi* pLeft;
-	lop_tin_chi* pRight;
+	lop_tin_chi *pLeft;
+	lop_tin_chi *pRight;
 };
-typedef struct lop_tin_chi* tree;
+typedef struct lop_tin_chi *tree;
 
-struct ds_lop_tin_chi {
+struct ds_lop_tin_chi
+{
 	tree ds = NULL;
 	int n = 0;
 
@@ -77,17 +81,19 @@ typedef struct ds_lop_tin_chi DS_LOP_TIN_CHI;
 // ===== END LOP TIN CHI =====
 
 // ===== BEGIN DS DANG KY =====
-struct dang_ky {
+struct dang_ky
+{
 	// data
 	char MASV[12];
 	float DIEM;
 	// pointer
-	dang_ky* pNext;
+	dang_ky *pNext;
 };
 typedef struct dang_ky DANG_KY;
 
-struct ds_dang_ky {
-	DANG_KY* pHead = NULL;
+struct ds_dang_ky
+{
+	DANG_KY *pHead = NULL;
 	int n = 0;
 };
 typedef struct dang_ky DS_DANG_KY;
@@ -97,11 +103,26 @@ typedef struct dang_ky DS_DANG_KY;
 
 void SortString() {}
 
-bool CheckExistMSSV(DS_SINH_VIEN ds_sv, char* massv) {
+bool CheckExistMSSV(DS_SINH_VIEN ds_sv, char *massv)
+{
 	bool exist = false;
-	for (SINH_VIEN* k = ds_sv.pHead; k != NULL; k = k->pNext)
+	for (SINH_VIEN *k = ds_sv.pHead; k != NULL; k = k->pNext)
 	{
-		if (_strcmpi(k->MASV, massv) == 0) {
+		if (_strcmpi(k->MASV, massv) == 0)
+		{
+			exist = true;
+			break;
+		}
+	}
+	return exist;
+}
+bool CheckExistMaMH(DS_MON_HOC ds_mh, char *maMH)
+{
+	bool exist = false;
+	for (int i = 0; i < ds_mh.n; i++)
+	{
+		if (_strcmpi(ds_mh.ds[i]->MAMH, maMH) == 0)
+		{
 			exist = true;
 			break;
 		}
@@ -109,7 +130,8 @@ bool CheckExistMSSV(DS_SINH_VIEN ds_sv, char* massv) {
 	return exist;
 }
 
-string RandomID(DS_SINH_VIEN ds_sv) {
+string RandomID(DS_SINH_VIEN ds_sv)
+{
 	string randomId = "MSSV0000";
 	char cloned[12];
 	// max 11 ki tu, con lai chua ki tu ket thuc chuoi.
@@ -124,21 +146,41 @@ string RandomID(DS_SINH_VIEN ds_sv) {
 	return randomId;
 }
 
+string RandomIDMH(DS_MON_HOC ds_mon_hoc)
+{
+	string randomId = "MH0000";
+	char cloned[12];
+	// max 11 ki tu, con lai chua ki tu ket thuc chuoi.
+	strcpy_s(cloned, 11, randomId.c_str());
+	do
+	{
+		for (int i = 4; i < randomId.length(); i++)
+		{
+			randomId[i] = rand() % (57 - 48 + 1) + 48;
+		}
+	} while (CheckExistMaMH(ds_mon_hoc, cloned));
+	return randomId;
+}
+
 // ===== END HELPER =====
 
 // SINH VIEN
-void Init_DS_Sinh_Vien(DS_SINH_VIEN& ds) {
+void Init_DS_Sinh_Vien(DS_SINH_VIEN &ds)
+{
 	ds.pHead = NULL;
 	ds.pTail = NULL;
 }
 
-bool isEmptyDSSV(DS_SINH_VIEN ds_sv) {
-	if (ds_sv.pHead == NULL && ds_sv.pTail == NULL) return true;
+bool isEmptyDSSV(DS_SINH_VIEN ds_sv)
+{
+	if (ds_sv.pHead == NULL && ds_sv.pTail == NULL)
+		return true;
 	return false;
 }
 
 // Insert and order by MALOP+MASV
-void InsertLastSv(DS_SINH_VIEN& ds_sinh_vien, SINH_VIEN* sv) {
+void InsertLastSv(DS_SINH_VIEN &ds_sinh_vien, SINH_VIEN *sv)
+{
 	if (ds_sinh_vien.pHead == NULL)
 	{
 		ds_sinh_vien.pHead = ds_sinh_vien.pTail = sv;
@@ -151,7 +193,7 @@ void InsertLastSv(DS_SINH_VIEN& ds_sinh_vien, SINH_VIEN* sv) {
 	}
 }
 
-void InsertFirstSv(DS_SINH_VIEN& ds_sv, SINH_VIEN* sv)
+void InsertFirstSv(DS_SINH_VIEN &ds_sv, SINH_VIEN *sv)
 {
 	if (ds_sv.pHead == NULL)
 	{
@@ -164,26 +206,28 @@ void InsertFirstSv(DS_SINH_VIEN& ds_sv, SINH_VIEN* sv)
 	}
 }
 
-void RemoveFirst(DS_SINH_VIEN& ds_sv)
+void RemoveFirst(DS_SINH_VIEN &ds_sv)
 {
-	SINH_VIEN* p;
-	if (ds_sv.pHead == NULL) return;
+	SINH_VIEN *p;
+	if (ds_sv.pHead == NULL)
+		return;
 
 	p = ds_sv.pHead;
 	ds_sv.pHead = p->pNext;
 	delete p;
 }
 
-void RemoveLast(DS_SINH_VIEN& ds_sv)
+void RemoveLast(DS_SINH_VIEN &ds_sv)
 {
-	if (ds_sv.pHead == NULL) return;
+	if (ds_sv.pHead == NULL)
+		return;
 
 	if (ds_sv.pHead->pNext == NULL)
 	{
 		RemoveFirst(ds_sv);
 		return;
 	}
-	for (SINH_VIEN* k = ds_sv.pHead; k != NULL; k = k->pNext)
+	for (SINH_VIEN *k = ds_sv.pHead; k != NULL; k = k->pNext)
 	{
 		if (k->pNext == ds_sv.pTail)
 		{
@@ -195,28 +239,33 @@ void RemoveLast(DS_SINH_VIEN& ds_sv)
 	}
 }
 
-void RemoveSvByMSSV(DS_SINH_VIEN& ds_sv, char* massv) {
-	if (isEmptyDSSV(ds_sv)) {
+void RemoveSvByMSSV(DS_SINH_VIEN &ds_sv, char *massv)
+{
+	if (isEmptyDSSV(ds_sv))
+	{
 		cout << "Danh sach dang rong. Khong the thuc hien chuc nang nay!" << endl;
 		return;
 	}
-	if (_strcmpi(ds_sv.pHead->MASV, massv) == 0) {
+	if (_strcmpi(ds_sv.pHead->MASV, massv) == 0)
+	{
 		RemoveFirst(ds_sv);
 		cout << "Xoa thanh cong vi tri dau tien" << endl;
 		system("pause");
 		return;
 	}
-	if (_strcmpi(ds_sv.pTail->MASV, massv) == 0) {
+	if (_strcmpi(ds_sv.pTail->MASV, massv) == 0)
+	{
 		RemoveLast(ds_sv);
 		cout << "Xoa thanh cong vi tri cuoi cung trong ds" << endl;
 		system("pause");
 		return;
 	}
 	bool exists = false;
-	SINH_VIEN* g = new SINH_VIEN;
-	for (SINH_VIEN* k = ds_sv.pHead; k != NULL; k = k->pNext)
+	SINH_VIEN *g = new SINH_VIEN;
+	for (SINH_VIEN *k = ds_sv.pHead; k != NULL; k = k->pNext)
 	{
-		if (_strcmpi(k->MASV, massv) == 0) {
+		if (_strcmpi(k->MASV, massv) == 0)
+		{
 			exists = true;
 			g->pNext = k->pNext;
 			delete k;
@@ -226,31 +275,39 @@ void RemoveSvByMSSV(DS_SINH_VIEN& ds_sv, char* massv) {
 		}
 		g = k;
 	}
-	if (!exists) cout << "\nKhong tim thay sinh vien" << endl;
+	if (!exists)
+		cout << "\nKhong tim thay sinh vien" << endl;
 }
 
-void Input_Sinh_Vien(DS_SINH_VIEN& ds_sinh_vien) {
+void Input_Sinh_Vien(DS_SINH_VIEN &ds_sinh_vien)
+{
 	int slsv;
 	int count = 0;
 	char maLop[15];
-	cout << "Nhap sl sv: "; cin >> slsv;
+	cout << "Nhap sl sv: ";
+	cin >> slsv;
 	cin.ignore();
-	cout << "\nNhap ma lop: "; gets_s(maLop);
+	cout << "\nNhap ma lop: ";
+	gets_s(maLop);
 	do
 	{
-		SINH_VIEN* sv = new SINH_VIEN;
-		if (sv == NULL) {
+		SINH_VIEN *sv = new SINH_VIEN;
+		if (sv == NULL)
+		{
 			cout << "Cap phat vung nho de khoi tao sv that bai." << endl;
 			system("pause");
 			return;
 		}
-		cout << "\nNhap ho: ";  getline(cin, sv->HO);
-		cout << "\nNhap ten: ";  getline(cin, sv->TEN);
+		cout << "\nNhap ho: ";
+		getline(cin, sv->HO);
+		cout << "\nNhap ten: ";
+		getline(cin, sv->TEN);
 		cout << "\nNhap gioi tinh: ";
 		gets_s(sv->PHAI);
 		cout << "\nNhap so dien thoai: ";
 		gets_s(sv->SDT);
-		cout << "\nNhap nam hoc: "; cin >> sv->NAMNHAPHOC;
+		cout << "\nNhap nam hoc: ";
+		cin >> sv->NAMNHAPHOC;
 		strcpy_s(sv->MALOP, 14, maLop);
 		cin.ignore();
 		strcpy_s(sv->MASV, 11, RandomID(ds_sinh_vien).c_str());
@@ -260,9 +317,10 @@ void Input_Sinh_Vien(DS_SINH_VIEN& ds_sinh_vien) {
 	} while (count < slsv);
 }
 
-void Show_DS_Sinh_Vien(DS_SINH_VIEN ds_sv) {
+void Show_DS_Sinh_Vien(DS_SINH_VIEN ds_sv)
+{
 	int total = 0;
-	for (SINH_VIEN* p = ds_sv.pHead; p != NULL; p = p->pNext)
+	for (SINH_VIEN *p = ds_sv.pHead; p != NULL; p = p->pNext)
 	{
 		cout << "========== SINH VIEN " << ++total << " ==========" << endl;
 		cout << "Ma lop: " << p->MALOP << endl;
@@ -277,25 +335,49 @@ void Show_DS_Sinh_Vien(DS_SINH_VIEN ds_sv) {
 //  END SINH VIEN
 
 // MON HOC
-void Insert_MonHoc(DS_MON_HOC& ds_mon_hoc) {
-	MON_HOC* monhoc = new MON_HOC;
-	fflush(stdin);
-	cout << "\nNhap vao ma mon hoc:";
-	gets_s(monhoc->MAMH);
+void Insert_MonHoc(DS_MON_HOC &ds_mon_hoc)
+{
+	for (int i = 0; i < 2; i++)
+	{
+		MON_HOC *monhoc = new MON_HOC;
+		strcpy_s(monhoc->MAMH, 11, RandomIDMH(ds_mon_hoc).c_str());
+		cout << "\nNhap vao ten mon hoc:";
+		gets_s(monhoc->TENMH);
+		if (strcmp(monhoc->TENMH, "") == 0)
+		{
+			cout << "\nLoi. Ten mon hoc khong hop le." << endl;
+			system("pause");
+		}
+		cout << "\nNhap vao so tin chi ly thuyet:";
+		cin >> monhoc->STCLT;
 
-	cout << "\nNhap vao ten mon hoc:";
-	gets_s(monhoc->TENMH);
-	cout << "\nNhap vao so tin chi ly thuyet:";
-	cin >> monhoc->STCLT;
-
-	cout << "\nNhap vao so tin chi thuc hanh:";
-	cin >> monhoc->STCTH;
-
-	ds_mon_hoc.ds[ds_mon_hoc.n] = monhoc;
-	ds_mon_hoc.n++;
+		cout << "\nNhap vao so tin chi thuc hanh:";
+		cin >> monhoc->STCTH;
+		cin.ignore();
+		ds_mon_hoc.ds[ds_mon_hoc.n] = monhoc;
+		ds_mon_hoc.n++;
+	}
 }
 
-void Show_DS_MonHoc(DS_MON_HOC dsMonHoc) {
+void RemoveMonHoc(DS_MON_HOC &ds_mon_hoc, char *maMH)
+{
+	if (ds_mon_hoc.n == 0)
+	{
+		cout << "\nDanh sach mon hoc rong. Khong the thuc hien chuc nang nay!!!" << endl;
+		return;
+	}
+	for (int i = 0; i < ds_mon_hoc.n; i++)
+	{
+		if (_strcmpi(ds_mon_hoc.ds[i]->MAMH, maMH) == 0)
+		{
+			ds_mon_hoc.ds[i] = ds_mon_hoc.ds[i + 1];
+		}
+		ds_mon_hoc.n--;
+	}
+}
+
+void Show_DS_MonHoc(DS_MON_HOC dsMonHoc)
+{
 	for (int i = 0; i < dsMonHoc.n; i++)
 	{
 		cout << "Ma mon hoc: " << dsMonHoc.ds[i]->MAMH << endl;
@@ -306,20 +388,21 @@ void Show_DS_MonHoc(DS_MON_HOC dsMonHoc) {
 }
 // END MON HOC
 
-
 // HANDLE FILES
 
-void Read_File_MonHoc(DS_MON_HOC& ds_mon_hoc) {
+void Read_File_MonHoc(DS_MON_HOC &ds_mon_hoc)
+{
 	ifstream fileIn;
 	fileIn.open("C:\\Users\\Nguyen Dai\\source\\repos\\QLDIEMSV_HTC\\QLDIEMSV_HTC\\DS_MONHOC.txt", ios_base::in);
-	if (fileIn.fail()) {
+	if (fileIn.fail())
+	{
 		cout << "Open file (DS_MONHOC.txt) failed. Please check and update path of file again !!!" << endl;
 		return;
 	};
 	while (!fileIn.eof())
 	{
 		char s[1];
-		MON_HOC* data = new MON_HOC;
+		MON_HOC *data = new MON_HOC;
 		fileIn.getline(data->MAMH, 12, ',');
 		fileIn.getline(data->TENMH, 256, ',');
 		fileIn >> data->STCLT;
@@ -338,17 +421,19 @@ void Read_File_MonHoc(DS_MON_HOC& ds_mon_hoc) {
 // TODO:
 void WriteFileMonHoc() {}
 
-void ReadFileSinhVien(DS_SINH_VIEN& ds_sv) {
+void ReadFileSinhVien(DS_SINH_VIEN &ds_sv)
+{
 	ifstream fileIn;
 	fileIn.open("C:\\Users\\Nguyen Dai\\source\\repos\\QUANLY_DIEM_SV\\QUANLY_DIEM_SV\\DS_SINH_VIEN.txt", ios_base::in);
-	if (fileIn.fail()) {
+	if (fileIn.fail())
+	{
 		cout << "Open file (DS_SINH_VIEN.txt) failed. Please check and update path of file again !!!" << endl;
 		return;
 	};
 	while (!fileIn.eof())
 	{
 		char s[1];
-		SINH_VIEN* data = new SINH_VIEN;
+		SINH_VIEN *data = new SINH_VIEN;
 		fileIn.getline(data->MALOP, 15, ',');
 		fileIn.getline(data->MASV, 12, ',');
 		getline(fileIn, data->HO, ',');
@@ -362,16 +447,16 @@ void ReadFileSinhVien(DS_SINH_VIEN& ds_sv) {
 	fileIn.close();
 }
 
-
 // END HANDLE FILES
 
-int main() {
+int main()
+{
 	srand(time(NULL));
 	DS_MON_HOC dsMonHoc;
 	DS_SINH_VIEN dsSinhVien;
-	Init_DS_Sinh_Vien(dsSinhVien);
+	/*Init_DS_Sinh_Vien(dsSinhVien);
 	Input_Sinh_Vien(dsSinhVien);
-	Show_DS_Sinh_Vien(dsSinhVien);
+	Show_DS_Sinh_Vien(dsSinhVien);*/
 	/*ReadFileSinhVien(dsSinhVien);
 
 	char str[12];
@@ -380,9 +465,14 @@ int main() {
 	RemoveSvByMSSV(dsSinhVien, str);
 	Show_DS_Sinh_Vien(dsSinhVien);*/
 	//Insert_MonHoc(dsMonHoc);
-	/*Read_File_MonHoc(dsMonHoc);
-	Show_DS_MonHoc(dsMonHoc);*/
-
+	/*Read_File_MonHoc(dsMonHoc);*/
+	//Show_DS_MonHoc(dsMonHoc);
+	cout << "\n=======================" << endl;
+	char mh[12];
+	cout << "\nNhap ma mon hoc can xoa: ";
+	gets_s(mh, 11);
+	RemoveMonHoc(dsMonHoc, mh);
+	Show_DS_MonHoc(dsMonHoc);
 	system("pause");
 	return 0;
 }
